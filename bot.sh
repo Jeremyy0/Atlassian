e96905c5c497d46acfda836441ca8ccf9cd72ac6
Jeremyy0 <52049010+Jeremyy0@users.noreply.github.com>
2019-06-20 19:49:31 -0400

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until `/usr/bin/node ~/Atlassian/bot.js`; do
+until `/usr/bin/node ~/AtlassianBot/bot.js`; do
     echo "Atlassian Bot crashed with exit code $?.  Respawning.." >/dev/null 2>&1
     sleep 1
 done
