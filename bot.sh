725e4e6f13d26816b6b44001ebe1cefe81302aa4
Jeremyy0 <52049010+Jeremyy0@users.noreply.github.com>
2019-06-20 20:07:53 -0400

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+npm install discord.io winston -save
+
 until `/usr/bin/node ~/AtlassianBot/bot.js`; do
     echo "Atlassian Bot crashed with exit code $?.  Respawning.." >/dev/null 2>&1
     sleep 1
