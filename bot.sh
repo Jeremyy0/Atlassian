0f26bd03f8f750a2b078538bd447d623bd69d27f
Jeremyy0 <52049010+Jeremyy0@users.noreply.github.com>
2019-06-20 20:20:40 -0400

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$( dirname "${BASH_SOURCE[0]}" )"
 npm install discord.io winston -save
 
 until `/usr/bin/node ~/AtlassianBot/bot.js`; do
