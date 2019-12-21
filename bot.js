3c1acf4ee85b13fb29512078f8e63e0dd4498135
Jeremyy0 <52049010+Jeremyy0@users.noreply.github.com>
2019-12-21 01:43:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
 //New Command Development
   if(cmd === "trigger") {
-    if (member.hasPermission('MANAGE_PERMISSIONS', false, false)) {  
+    if (userID.hasPermission('MANAGE_PERMISSIONS', false, false)) {  
       bot.sendMessage({to: channelID,message: "yeet"});;
     }
   }
