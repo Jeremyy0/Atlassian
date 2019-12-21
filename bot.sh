422914c5c2687e0b67e0f8f0f17c60c4c6d4c375
Jeremyy0 <52049010+Jeremyy0@users.noreply.github.com>
2019-12-21 15:06:14 -0500

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -3,7 +3,6 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 npm install discord.io winston mysql -save
 npm install https://github.com/woor/discord.io/tarball/gateway_v6
-npm install discord.js
 
 until `/usr/bin/node ~/AtlassianBot/bot.js`; do
     echo "Atlassian Bot crashed with exit code $?.  Respawning.." >/dev/null 2>&1
