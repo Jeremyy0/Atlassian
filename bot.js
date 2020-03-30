aa9e651d683212f0a6d3d6d06038e6e4670c4ce8
Jeremy <jeremy.good@tuxperience.com>
2020-03-30 01:45:42 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,12 +56,12 @@ bot.on('message', function (user, userID, channelID, message, evt) {
   }
 
   //Search resource tile locations
-search:  if(cmd === "find" || cmd === "whereis") {
+if(cmd === "find" || cmd === "whereis") {
     con.query("select tiles from (SELECT resource, GROUP_CONCAT(distinct tile separator ', ') as tiles from locations group by resource) as T where resource=\""+[args[0].toLowerCase()]+"\"", function (err, result, fields) {
     if (err) bot.sendMessage({to: channelID,message: "\`\`\`Ahoy! Thar\'s an error with yer command fix yer syntax and try again.\`\`\`"});;
       if (result[0]) {
         bot.sendMessage({to: channelID,message: "\`\`\`"+args[0]+" can be found in the following tiles: " + result[0].tiles+"\`\`\`"});
-        break search;
+        return;
       }
     });
 
