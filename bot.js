c3d8ee2c2e4789a9104c57d04890a0f5de189ac2
Jeremy <jeremy.good@tuxperience.com>
2020-04-18 08:29:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,39 +14,38 @@ client.on('message', msg => {
     var args = msg.content.substring(1).split(' ');
     var cmd = args[0];
     args = args.splice(1);
-  }
-  if (cmd === 'ping') {
-    msg.reply('Pong!');
-  }
-  if (cmd === 'test') {
-    if(msg.member.hasPermission("ADMINISTRATOR")) {
-      msg.channel.send('Success!');
 
+    if (cmd === 'ping') {
+      msg.reply('Pong!');
     }
-  }
+    if (cmd === 'test') {
+      if(msg.member.hasPermission("ADMINISTRATOR")) {
+        msg.channel.send('Success!');
+      }
+    }
 
-  //Search resource tile locations
-  if(cmd === 'find' || cmd === prefix+'whereis') {
-    var found = false;
-    con.query("select tiles from (SELECT resource, GROUP_CONCAT(distinct tile separator ', ') as tiles from locations group by resource) as T where resource=\""+[args[0].toLowerCase()]+"\"", function (err, result, fields) {
-      if (err) msg.channel.send("\`\`\`Ahoy! Thar\'s an error with yer command fix yer syntax and try again.\`\`\`");;
-        if (result[0]) {
-          msg.channel.send("\`\`\`"+args[0]+" can be found in the following tiles: " + result[0].tiles+"\`\`\`");
-          found = true;
-        }
-      });
-      con.query("select tiles from (SELECT animal, GROUP_CONCAT(distinct tile separator ', ') as tiles from animals group by animal) as T where animal=\""+[args[0].toLowerCase()]+"\"", function (err, result, fields) {
+    //Search resource tile locations
+    if(cmd === 'find' || cmd === prefix+'whereis') {
+      var found = false;
+      con.query("select tiles from (SELECT resource, GROUP_CONCAT(distinct tile separator ', ') as tiles from locations group by resource) as T where resource=\""+[args[0].toLowerCase()]+"\"", function (err, result, fields) {
         if (err) msg.channel.send("\`\`\`Ahoy! Thar\'s an error with yer command fix yer syntax and try again.\`\`\`");;
           if (result[0]) {
-            msg.channel.send({"\`\`\`"+args[0]+" can be found in the following tiles: " + result[0].tiles+"\`\`\`");
-          } else {
-            if (!(found)) {
-              msg.channel.send({"\`\`\`"+args[0]+"\'s locations are not known to me.\`\`\`");;
-            }
+            msg.channel.send("\`\`\`"+args[0]+" can be found in the following tiles: " + result[0].tiles+"\`\`\`");
+            found = true;
           }
-      });
+        });
+        con.query("select tiles from (SELECT animal, GROUP_CONCAT(distinct tile separator ', ') as tiles from animals group by animal) as T where animal=\""+[args[0].toLowerCase()]+"\"", function (err, result, fields) {
+          if (err) msg.channel.send("\`\`\`Ahoy! Thar\'s an error with yer command fix yer syntax and try again.\`\`\`");;
+            if (result[0]) {
+              msg.channel.send({"\`\`\`"+args[0]+" can be found in the following tiles: " + result[0].tiles+"\`\`\`");
+            if (!(found)) {
+              } else {
+                msg.channel.send({"\`\`\`"+args[0]+"\'s locations are not known to me.\`\`\`");;
+              }
+            }
+        });
+    }
   }
-
 });
 
 client.login('token');
