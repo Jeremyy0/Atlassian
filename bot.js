755fdaf6b64d932404a40414c136e598d7df558d
Jeremy <jeremy.good@tuxperience.com>
2020-04-18 08:39:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,14 @@ var config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Initialize Database
+var mysql = require('mysql');
+var con = mysql.createConnection(config.db);
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("DB Connected!");
+});
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
