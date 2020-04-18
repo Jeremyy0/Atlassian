4ba956febb68b61b1dcfabbb58f745b95dbab6b6
Jeremy <jeremy.good@tuxperience.com>
2020-04-18 07:13:37 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,11 @@ const DiscordJS = require('discord.js');
 const bot = new Discord.Client();
 
 // Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+//logger.remove(logger.transports.Console);
+//logger.add(new logger.transports.Console, {
+//    colorize: true
+//});
+//logger.level = 'debug';
 
 // Initialize Database
 var mysql = require('mysql');
@@ -44,12 +44,13 @@ function choose(choices) {
 
 
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
+//bot.on('ready', function (evt) {
+//    logger.info('Connected');
+//    logger.info('Logged in as: ');
+//    logger.info(bot.username + ' - (' + bot.id + ')');
+//});
+
+bot.on('message', message => {
 
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
@@ -221,4 +222,4 @@ bot.on('message', function (user, userID, channelID, message, evt) {
   }
 
 
-})
+});
